7adcb485822d9f2a1d2f418631c7143087d56149
Sk. Sanjidul Haqu <sanjidul@xpertsolvers.com>
2023-03-25 23:53:35 +0600

    search product dynamic product showing in page implemented.

--- src/app/search/search.component.ts
+++ src/app/search/search.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
+import { product } from '../data-types';
+import { ProductService } from '../services/product.service';
 
 @Component({
   selector: 'app-search',
@@ -6,10 +9,14 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./search.component.css']
 })
 export class SearchComponent implements OnInit {
-
-  constructor() { }
+searchResult: undefined | product[];
+  constructor(private activateRoute: ActivatedRoute, private product: ProductService) { }
 
   ngOnInit(): void {
+  let query = this.activateRoute.snapshot.paramMap.get('query');
+  query && this.product.searchProducts(query).subscribe(result =>{
+    this.searchResult = result; 
+  })
   }
 
 }
