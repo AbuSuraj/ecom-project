c579ee19b645f869ca28ae54508ab8dafddca5a7
Abu Suraj <abusurajbd@gmail.com>
2023-03-29 23:11:18 +0600

    remove unused code

--- src/app/services/user.service.ts
+++ src/app/services/user.service.ts
@@ -11,7 +11,7 @@ export class UserService {
   constructor(private http: HttpClient, private router: Router) { }
   userSignUp(user:SignUp){ 
     this.http.post("http://localhost:3000/users",user,{observe: 'response'}).subscribe(result=>{
-      console.log(result);
+    
       if(result){
         localStorage.setItem("user", JSON.stringify(result.body));
         this.router.navigate(['/']);
